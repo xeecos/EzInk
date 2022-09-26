a24ace48ae756351651a1c8ceed5565d4055535b
虎子哥 <flashindream@gmail.com>
2022-09-26 18:06:10 +0800

    update

--- src/src.ino
+++ src/src.ino
@@ -160,7 +160,31 @@ Epd epd;
 //     epd.DisplayFrame();
 //     epd.DisplayFrame();
 // }
-
+/**
+ * @file src.ino
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-26
+ * 
+ * @copyright Copyright (c) 2022
+ * 气象页  https://www.52stl.com/wp-content/uploads/52stl.com_hero-wparts.jpg
+ * 日期
+ * 时间
+ * 天气/气温
+ * 72小时天气
+ * 日出日落时间 月相  月出月落时间
+ * 实时数据页
+ * 温度曲线
+ * 湿度曲线
+ * 气压曲线
+ * 观星指数页
+ * 指数  月相
+ * 天气 湿度  温度  风速
+ * 光污染等级
+ * 无月时间
+ * 银河时间
+ */
 #include <Arduino.h>
 #include <cppsrc/U8g2lib.h>
 HWCDC USBSerial;
